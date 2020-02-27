63129720886ebfd5fda6307e124c200e7654c0cf
Gosline <sara.gosline@pnnl.gov>
2020-02-27 13:18:47 -0800

    moved analysis to RMd

--- bin/runGilteritinibAnalysis.R
+++
@@ -1,8 +0,0 @@
-#do gilteritinib analysis
-
-library(amlresistancenetworks)
-
-#lods the gilterinib data to analyze similarities
-readRDS(system.file('gilteritinibData.Rds',package='amlresistancenetworks'))
-
-#step 1, compute LFC of FGF2, FL in early gilterinitib samples for both cell lines
