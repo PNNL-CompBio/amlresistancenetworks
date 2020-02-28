3d0c802f48edbc70d4923d72b6a4b2c13ec25c2d
Gosline <sara.gosline@pnnl.gov>
2020-02-28 14:50:35 -0800

    working on getting the analysis to visualize

--- R/proteomicStats.R
+++ R/proteomicStats.R
@@ -10,7 +10,7 @@ computeProteinFC<-function(tidied.df){
   
 }
 
-#' @require PCSF
+#' @import PCSF
 #' @export
 computeProteinNetwork<-function(tidied.df){
   require(PCSF)
@@ -37,10 +37,32 @@ computeNetworkDifferences<-function(network1,network2){
 
 #' compute gene set enrichment
 #' @export
-#' @requires clusterProfiler
-#' @param output of genes and difference values
+#' @importFrom clusterProfiler GSEA
+#' @importFrom msigdbr msigdbr
+#' @import org.Hs.eg.db
+#' @param genes.with.values of genes and difference values
+#' @param prot.univ the space of all proteins we are considering
 #' @return gSEA output type stuff
-computeGSEA<-function(genes.with.values){
-  require(clusterProfiler)
-}
+computeGSEA<-function(genes.with.values,prot.univ){
+
+    require(org.Hs.eg.db)
+  genes.with.values<-genes.with.values%>%
+      rename(Gene="SYMBOL")%>%
+    left_join(AnnotationDbi::select(org.Hs.eg.db,keys=keys(org.Hs.eg.db,'ENTREZID'),columns=c("ENTREZID","SYMBOL"),by='SYMBOL'))
+  
+    genes.with.values<-genes.with.values%>%arrange(desc(value))
+    
+  genelist=genes.with.values$value
+ # names(genelist)=genes.with.values$SYMBOL
+   names(genelist)=genes.with.values$ENTREZID
+  
+ # t2g= msigdbr::msigdbr(species="Homo sapiens",category="C2")%>%dplyr::select(gs_id,human_gene_symbol)
+    t2g= msigdbr::msigdbr(species="Homo sapiens",category="C5")%>%
+      subset(human_gene_symbol%in%prot.univ)%>%
+               dplyr::select(gs_name,entrez_gene)
+  #  t2n =msigdbr::msigdbr(species="Homo sapiens",category="C2")%>%dplyr::select(gs_id,gs_name)
+  
+  gr=clusterProfiler::GSEA(genelist[!is.na(genelist)],TERM2GENE = t2g,pAdjustMethod = 'fdr')
+    return(gr)
+  }
 
