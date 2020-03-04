55f76d12201776d0ea1d900a13ed573324d24fef
Gosline <sara.gosline@pnnl.gov>
2020-03-03 21:57:26 -0800

    updated

--- R/dataManagement.R
+++ R/dataManagement.R
@@ -43,3 +43,59 @@ saveRDS(gilteritinib.data,file='inst/gilteritinibData.Rds')
   return(gilteritinib.data)
 
     }
+
+
+
+#' Reads in data frame and computes fold change
+#' @param data.frame
+#' @return tidied data frame with p-values and fold change
+#' @import dplyr
+#' @export
+computeFoldChangePvals<-function(gilt.data,
+                                  treatmentCond='Early Gilteritnib',
+                                 ligands=c("FL","FGF2")){
+  
+  data<-gilt.data%>%
+    subset(treatment%in%(c('None',treatmentCond)))%>%
+    dplyr::select(CellLine,Gene,value,ligand)%>%
+    subset(!is.na(value))
+  
+
+  #remove those data that have only 1 replicate  
+  reps<-data%>%
+    group_by(CellLine,Gene,ligand)%>%
+    mutate(reps=length(value))%>%
+    subset(reps>1)%>%
+    ungroup()
+  
+  res<-lapply(ligands,function(lig){
+    #compute the significance for FL-modulaetd changes in both cell lines
+    vals=reps%>%
+      group_by(CellLine,Gene)%>%
+      subset(ligand%in%c('None',lig))%>%
+      mutate(ligand=as.factor(ligand))%>%
+      mutate(conds=n_distinct(ligand))%>%
+      subset(conds>1)
+    
+    signif=vals%>%
+      group_modify(~infer::t_test(.,value~ligand,order=c(lig,"None")),keep=TRUE)%>%
+      dplyr::select(c(CellLine,Gene,p_value))%>%
+      ungroup()%>%
+      group_by(CellLine)%>%
+      mutate(p_adj=p.adjust(p_value))%>%
+      ungroup()%>%
+      rename(p_value=paste(lig,'pVal',sep='_'),
+             p_adj=paste(lig,'pAdj',sep='_'))
+    
+    
+  })
+  ##now compute the significance for FGF2 treated cells
+  
+
+  mean.diffs<-data%>%group_by(CellLine,Gene,ligand)%>%
+    summarize(meanVal=mean(value))%>%
+    tidyr::pivot_wider(names_from=ligand,values_from = meanVal)%>%
+    mutate(FGF2_to_parent=FGF2-None,FL_to_parent=FL-None)%>%
+    full_join(fl.signif,by=c('CellLine','Gene'))%>%
+    full_join(fgf.signif,by=c('CellLine','Gene'))
+}
