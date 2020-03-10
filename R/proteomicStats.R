d267b511e1dcb5b1f1ce53a1e2b55d82228a1c2d
Sara JC Gosline <sara.gosline@pnnl.gov>
2020-03-09 20:55:18 -0700

    updated

--- R/proteomicStats.R
+++ R/proteomicStats.R
@@ -1,19 +1,22 @@
 ##proteomic stats
 
 
-#' compute protein FC
-#' @export
-#' @param tidied data frame
-#' @param condition of interest
-#' @return data frame with 3 columns - genes with LFC values and significance values
-computeProteinFC<-function(tidied.df){
-  
-}
-
 #' @import PCSF
 #' @export
-computeProteinNetwork<-function(tidied.df){
+#' 
+computeProteinNetwork<-function(terms,nrand=100){
   require(PCSF)
+  
+  data("STRING")
+  
+  ppi <- construct_interactome(STRING)
+  
+ names(terms)<-vals$Gene
+  
+  
+  subnet <- PCSF_rand(ppi, terms, n=nrand, r=0.3,w = 4, b = 50, mu = 0.0005)
+  
+  subnet  
 }
 
 
@@ -35,6 +38,72 @@ computeNetworkDifferences<-function(network1,network2){
   
 }
 
+
+
+
+#' Reads in data frame and computes fold change
+#' 
+#' @export
+#' @param data.frame 
+#' @param treatmentCond
+#' @param ligands
+#' @return tidied data frame with p-values and fold change
+#' @import dplyr
+#' @import stringr
+#' @import tidyr
+#' @import purrr
+#' 
+computeFoldChangePvals<-function(g.data,
+                                 control='None',
+                                 conditions=c("FL","FGF2")){
+  
+  data<-g.data%>%
+    dplyr::select(CellLine,Gene,value,treatment)%>%
+    subset(!is.na(value))%>%
+    subset(!is.na(Gene))
+  
+  
+  #remove those data that have only 1 replicate  
+  reps<-data%>%
+    group_by(CellLine,Gene,treatment)%>%
+    dplyr::mutate(reps=length(value))%>%
+    subset(reps>1)%>%
+    ungroup()        
+  
+  ##this function iterates through every ligand of interest and compares it to 'None'
+  lig.signif<-purrr::map_df(conditions,function(lig){
+    #compute the significance for FL-modulaetd changes in both cell lines
+    reps%>%
+      subset(treatment%in%c(control,lig))%>% #get only those valuese that are 'nOne' or the name of the ligand
+      dplyr::group_by(Gene)%>% #group by cell line and gene to get per-gene values
+      dplyr::mutate(treatment=as.factor(treatment))%>% #change to factor for t-test
+      dplyr::mutate(conds=n_distinct(treatment))%>% subset(conds>1)%>% #only get those for which there are sufficient reps
+      dplyr::group_modify(~infer::t_test(.,value~treatment,order=c(lig,control)),keep=TRUE)%>% #calculate p-value
+      dplyr::select(c(Gene,p_value))%>%  #select the valuese of interest for us
+      ungroup()%>%
+    #  dplyr::group_by(CellLine)%>% #regroup so that we can correct the p-values
+      dplyr::mutate(p_adj=p.adjust(p_value),Condition=lig,Control=control)%>% #adjust
+      ungroup()
+  })
+  
+  lig.fc<-purrr::map_df(conditions,function(lig){
+    reps%>%
+      subset(treatment%in%c(control,lig))%>% #get only those valuese that are 'nOne' or the name of the ligand
+      dplyr::mutate(treatment=stringr::str_replace(treatment,lig,'condition'))%>% #rename so we can spread
+      dplyr::mutate(treatment=stringr::str_replace(treatment,control,'conVal'))%>% #rename so we can spread
+      ungroup()%>%dplyr::group_by(Gene,treatment)%>%
+      dplyr::summarize(meanVal=mean(value))%>% #calculate mean across cell line/gene/treatment
+      tidyr::pivot_wider(names_from=treatment,values_from = meanVal)%>% #spread to compute difference
+      dplyr::mutate(condition_to_control=condition-conVal,Condition=lig,Control=control)%>% #compute difference
+      dplyr::select(Gene,Condition,Control,condition_to_control)
+  })
+  lig.datasets<-
+    lig.signif%>%full_join(lig.fc,by=c('Gene','Condition','Control'))
+  
+  return(lig.datasets) 
+}
+
+
 #' compute gene set enrichment
 #' @export
 #' @importFrom clusterProfiler GSEA
@@ -47,7 +116,7 @@ computeGSEA<-function(genes.with.values,prot.univ){
 
     require(org.Hs.eg.db)
   genes.with.values<-genes.with.values%>%
-      rename(Gene="SYMBOL")%>%
+      rename(SYMBOL='Gene')%>%
     left_join(AnnotationDbi::select(org.Hs.eg.db,keys=keys(org.Hs.eg.db,'ENTREZID'),
                                     columns=c("ENTREZID","SYMBOL"),by='SYMBOL'))
   
