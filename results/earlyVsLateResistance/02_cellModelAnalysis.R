5275a651a4c6f8f53da6bba50414cef36ad3a860
Sara JC Gosline <sara.gosline@sagebase.org>
2020-06-24 09:59:25 -0700

    adding targetted analysis

--- results/earlyVsLateResistance/02_cellModelAnalysis.R
+++ results/earlyVsLateResistance/02_cellModelAnalysis.R
@@ -1,4 +1,4 @@
-##run through drug resistnace analysis
+##run through drug resistance analysis
 
 
 library(amlresistancenetworks)
@@ -9,6 +9,33 @@ gilt.data<-querySynapseTable('syn22156807')
   #readRDS(system.file('gilteritinibData.Rds',package='amlresistancenetworks'))
 prot.univ<-unique(gilt.data$Gene)
 
+syn<-synapseLogin()
+targ.data<-read.csv2(syn$get('syn22214245')$path,sep='\t',header=T)%>%
+  rename(Gene='T..Category')%>%
+  tidyr::pivot_longer(-Gene,names_to='sample',values_to='value')%>%
+  mutate(value=as.numeric(value))%>%
+  rowwise()%>%
+  mutate(samp2=stringr::str_replace(sample,'_3June20.+',''))%>%
+  mutate(Patient=stringr::str_replace(samp2,'.*Ex16_',''))%>%
+  select(-c(samp2,sample))
+
+norm.data<-targ.data%>%tidyr::pivot_wider(values_from=value,names_from=Patient)%>%tidyr::pivot_longer(-c(Gene,pool),values_to='value',names_to='Patient')%>%rowwise()%>%mutate(logRatio=log10(value)-log10(pool))%>%
+  dplyr::select(-c(pool,value))%>%
+  rename(value='logRatio')
+
+add.cols<-norm.data%>%
+  mutate(condition=stringr::str_replace(Patient,'[0-9]+',''))%>%
+  rename(Sample='Patient')%>%
+  mutate(ligand='none')%>%
+  mutate(cellLine='Patient')
+
+comb.data<-gilt.data%>%rename(condition='treatment')%>%
+  dplyr::select(Gene,value,Sample,condition,ligand,cellLine)%>%
+  subset(Gene%in%targ.data$Gene)%>%
+  rbind(add.cols)%>%
+  mutate(value=as.numeric(value))
+                                             
+ggplot(comb.data,aes(x=condition,fill=cellLine,y=value))+geom_boxplot()
 
 early.data<-gilt.data%>%
   subset(treatment%in%(c('None','Early Gilteritinib')))%>%
@@ -105,8 +132,8 @@ files=c('proteomics__MOLM14_None_vs_EarlyGilteritinib_gseaGO_result.txt','proteo
 
 
 
-rerunPrev=TRUE
-
+rerunPrev=FALSE
+if(rerunPrev){
   amlresistancenetworks::plotSingleProtein(gene='CPT1A',gilt.data,'abundance')
 #  amlresistancenetworks::plotSingleProtein(gene='AURKB',gilt.data,'bundance')
  # amlresistancenetworks::plotSingleProtein(gene='CUL5',gilt.data,'abundance')
@@ -125,9 +152,7 @@ rerunPrev=TRUE
    v.vs.parental<-plotDataByCondition(gilt.data,control='None',condition=c('Early Gilteritinib','Late Gilteritinib'),cellLine='MV411',doNetwork=dn)
   
    v.late.early<-plotDataByCondition(gilt.data,condition='Late Gilteritinib',control='Early Gilteritinib',cellLine='MV411',doNetwork=dn)
-  
- if(FALSE){ 
-  #mv411
+    #mv411
  
    
    sapply(c("FGF2","FLT3"),function(lig){
@@ -140,9 +165,11 @@ rerunPrev=TRUE
                                       cellLine='MV411',doNetwork=dn,prefix=lig)
    })
   
-}
+
 
 plotLeadingEdgeGenes(gdat=subset(gilt.data,cellLine=='MOLM14'),pathname='metab',respath=files[1],prefix='MOLM14_EarlyRes')
 plotLeadingEdgeGenes(gdat=subset(gilt.data,cellLine=='MOLM14'),pathname='lipid',respath=files[1],prefix='MOLM14_EarlyRes')
 plotLeadingEdgeGenes(gdat=subset(gilt.data,cellLine=='MV411'),pathname='metab',respath=files[2],prefix='MV411_EarlyRes')
 plotLeadingEdgeGenes(gdat=subset(gilt.data,cellLine=='MV411'),pathname='lipid',respath=files[2],prefix='MV411_EarlyRes')
+
+}
